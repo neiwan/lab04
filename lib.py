65e93e9f1734e23ca43d2c9d213f2d7cbab750c5
stormdesso <stormdesso@gmail.com>
2022-11-29 15:20:07 +0500

    test

--- lib.py
+++ lib.py
@@ -1,25 +1,18 @@
-N = int(input("Введите количество списков\n"))
-strings = []
-symbols = []
-repeats = []
-for i in range(N):
-    strings.append(input("Введите строку " + str(i + 1) + "\n"))
-for i in range(len(strings)):
-    stroka = strings[i]
-    for j in range(len(stroka)):
-        ECTb = False
-        for k in range(len(symbols)):
-            if stroka[j] == symbols[k]:
-                repeats[k] = True
-                ECTb = True
-        if not ECTb:
-            symbols.append(stroka[j])
-            repeats.append(False)
-            K = 0
-for i in range(len(repeats)):
-    if repeats[i]:
-        K += 1
-print(repeats)
-print(symbols)
-print(stroka)
-print("Количество повторяющихся символов: " + str(K))
+def lib(strings):
+    # N = int(input("Введите количество списков\n"))
+    # strings = []
+    repeats = []
+    dictionary = []
+    count = 0
+    # for i in range(N):
+    #     strings.append(input("Введите строку " + str(i + 1) + "\n"))
+    for i in range(len(strings)):
+        stroka = strings[i]
+        for j in range(len(stroka)):
+            if (stroka[j]) in dictionary:
+                count += 1
+                repeats.append(stroka[j])
+            else:
+                dictionary.append(stroka[j])
+    return repeats
+# print("Количество повторяющихся символов: ", len(repeats))
